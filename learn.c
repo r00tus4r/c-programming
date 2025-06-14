a2ec7349911d2674819f1e2ce08450fd8bfbdc90
Diyorbek Qodirboyev <diyorbek.qodirboyev05@gmail.com>
2025-06-14 20:29:14 +0500

    fix: main function fixed

--- learn.c
+++ learn.c
@@ -2,7 +2,12 @@
 
 #include <stdio.h>
 
-int main() {
+int main(int argc, const char * argv[]) {
 	printf("Hello World!\n");
+	printf("I love C!\n");
+	printf("My name is Diyorbek.\n");
+	printf("I'm 19 years old.\n");
+	printf("I'm a Programmer.\n");
+
 	return 0;
 }
